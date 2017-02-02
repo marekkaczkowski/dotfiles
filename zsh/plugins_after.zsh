3bf7e5fed21a26ab99c45c749312dbf71a82bda8
Marek Kaczkowski <marek.kaczkowski@gmail.com>
2017-02-02 22:05:38 +0100

    Update submodules 👑 Cleanup filenames

--- zsh/plugins_after.zsh
+++ zsh/plugins_after.zsh
@@ -3,8 +3,5 @@
 # Load oh-my-zsh
 source $ZSH/oh-my-zsh.sh
 
-# Load nvm
-source $HOME/.nvm/nvm.sh
-
 # Load Powerline
 #source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
