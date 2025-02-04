55dc2411003eba2ccebc924ada1730cde8e658ce
Marek Kaczkowski <marek.kaczkowski@gmail.com>
2025-02-04 18:02:06 +0100

    Initial commit

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "lalitmee/cobalt2.nvim",
+    event = { "ColorSchemePre" }, -- if you want to lazy load
+    dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
+    init = function()
+        require("colorbuddy").colorscheme("cobalt2")
+    end,
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cobalt2",
+    },
+  }
+}
---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+local config = require("config")
+require("events")
+require("tab-bar")
+-- TODO: symlink entire wezterm folder
+
+config.color_scheme = "Cobalt 2 (Gogh)"
+
+return config
